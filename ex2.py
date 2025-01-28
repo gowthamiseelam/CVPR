c31b228a53f6eb7bf657134831effcfc280ba6f1
gowthamiseelam <160471775+gowthamiseelam@users.noreply.github.com>
2025-01-28 14:11:10 +0530

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -7,4 +7,11 @@ draw.rectangle(xy=(100,100,500,500),
                width=40)
 img.show()
 
+import cv2
+img=cv2.imread('image.jpeg')
+wind_name='shape'
+image=cv2.rectangle(img,(50,50),(150,150),(255,255,0),10)
+cv2.imshow(wind_name,image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
